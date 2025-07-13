ea5ea087cfafe1a51108ddbb0c5c7c300893d00f
Gen <gsmgentech@gmail.com>
2025-07-13 14:01:43 +0800

    Add files via upload

--- js/technicians.js
+++ js/technicians.js
@@ -20,14 +20,14 @@ document.addEventListener('DOMContentLoaded', () => {
       name: "John Mark Ibana",
       role: "Technician",
       location: "PHILIPPINES | MANILA",
-      phone: "+63923965667",
+      phone: "+639923965667",
       image: "img/admin2.jpg",
       verified: true,
       shareLink: "gentechserver.com/technician/johnmarkibana",
       contacts: [
         { icon: "https://upload.wikimedia.org/wikipedia/commons/b/be/Facebook_Messenger_logo_2020.svg", link: "https://m.me/ibanajohnmark", label: "John Mark" },
         { icon: "https://cdn-icons-png.flaticon.com/512/733/733547.png", link: "https://www.facebook.com/ibanajohnmark", label: "John Mark" },
-        { icon: "https://cdn-icons-png.flaticon.com/512/733/733585.png", link: "https://wa.me/63923965667", label: "+63923965667" },
+        { icon: "https://cdn-icons-png.flaticon.com/512/733/733585.png", link: "https://wa.me/639923965667", label: "+639923965667" },
       ]
     },
   ];
