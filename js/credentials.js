6cd6260ee9eea8e480402b0423c9008a8c9f36c7
Gen <gsmgentech@gmail.com>
2025-09-29 22:09:51 +0800

    Delete js directory

--- js/credentials.js
+++
@@ -1,6 +0,0 @@
-// credentials.js
-// naka-hash na password gamit SHA-256
-const validCredentials = {
-  username: "Genrev",
-  passwordHash: "4a17b1563b4ef5c6e3b6fefb0d2f9b0e6f7c2c32455d2e0d6d9c0a3d4b6e8f90" 
-};
